5105cfb29ebccfef9af53d48a7dc7feddc2256b9
14AlejandroNauatDuran <alejandro.nauatduran@itsva.edu.mx>
2019-10-07 19:16:54 -0500

    funcion encriptar

--- proyecto/proyectoU1.js
+++ proyecto/proyectoU1.js
@@ -15,7 +15,23 @@ function obtenercontenido(){
 	texto.open("GET",nomArch,true);
 	texto.send();
 }
+document.getElementById("encriptar").addEventListener("click",encriptando);
 
+function encriptando(){
+	
+	var enc = document.getElementById("parrafo").innerHTML;
+	var remplazo = enc.replace(/[a|A]/g,"b");
+	remplazo = remplazo.replace(/[p|P]/g,"z");
+	remplazo = remplazo.replace(/[i|I]/g,"w");
+	remplazo = remplazo.replace(/[n|N]/g,"p");
+	remplazo = remplazo.replace(/[c|C]/g,"y");
+	remplazo = remplazo.replace(/[r|R]/g,"i");
+	remplazo = remplazo.replace(/[m|M]/g,"8");
+
+	document.getElementById("encriptado").innerHTML = remplazo;
+	
+	
+}
 
 }
 
